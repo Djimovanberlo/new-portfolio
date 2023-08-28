9e5f9c2355bbfe4f8354a25d0139fda2b0e3d116
Djimo van Berlo <djimovanberlo@gmail.com>
2023-08-28 21:51:34 +0200

    header

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Footer from 'layout/footer'
 import './styles/main.scss'
 import 'styles/app.scss'
 
+import { SectionNames } from 'interfaces/layout'
+
 const App = () => (
   <div className='app'>
-    {/* <Header /> */}
+    <Header sectionName={SectionNames.Banner} />
     <Main />
     {/* <Footer /> */}
   </div>
