409d6128b8bbf22e597ea106cc1e6ac7f9f660c2
Djimo van Berlo <djimovanberlo@gmail.com>
2023-08-28 18:13:29 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        // nav
+        // main
+        // footer
+        <div>
+          <h1>Hello World</h1>
+          <Link to='about'>About Us</Link>
+        </div>
+      ),
+    },
+    {
+      path: 'about',
+      element: <div>About</div>,
+    },
+  ])
+
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
