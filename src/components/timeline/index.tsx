3d24672bf956a2f329a2e170632ada405205df60
Djimo van Berlo <djimovanberlo@gmail.com>
2023-09-01 17:49:46 +0200

    timeline

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -1,19 +1,48 @@
-import Icon from 'components/icon'
-import { Tooltip } from 'components/tooltip'
+import { H2, P } from 'components/typography'
+import utrechtImg from 'assets/images/utrecht.png'
+import tromsoImg from 'assets/images/tromso.png'
+import amsterdamImg from 'assets/images/amsterdam.svg'
+import ollinImg from 'assets/images/olln.png'
 
-const TimelineIcon = ({ tooltipChildren }) => (
+const TimelineIcon = ({ tooltipChildren, icon }) => (
   <div className='timelineIcon'>
-    <Tooltip>{tooltipChildren}</Tooltip>
-    <Icon />
+    <P className='tooltip'>{tooltipChildren}</P>
+    <img src={icon} />
   </div>
 )
 
 const Timeline = () => {
-  const data = ['a', 'b', 'c'].map((item, index) => {
-    return <TimelineIcon key={index} tooltipChildren={item} />
+  const temp = [
+    {
+      tooltipText: 'Born and raised in Utrecht',
+      icon: utrechtImg,
+    },
+    {
+      tooltipText: 'Exchange student in Tromsø',
+      icon: tromsoImg,
+    },
+    {
+      tooltipText: 'Studies and work in Amsterdam',
+      icon: amsterdamImg,
+    },
+    {
+      tooltipText: 'Living in Louvain la Neuve while working remotely',
+      icon: ollinImg,
+    },
+  ]
+
+  const data = temp.flatMap(({ icon, tooltipText }, index) => {
+    const element = <TimelineIcon key={index} tooltipChildren={tooltipText} icon={icon} />
+    const separator = <div key={`hr-${index}`} className='timeline__line' />
+    return [element, separator]
   })
 
-  return <div>{data}</div>
+  return (
+    <div className='timeline'>
+      {data}
+      <H2>?</H2>
+    </div>
+  )
 }
 
 export default Timeline
