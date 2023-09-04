62b4c1937f6351bf1aca6731bd1f61edb116a2bc
Djimo van Berlo <djimovanberlo@gmail.com>
2023-09-04 20:28:55 +0200

    setup email service

--- src/components/contact-form/index.tsx
+++ src/components/contact-form/index.tsx
@@ -1,9 +1,31 @@
-import Button, { HoverButton } from 'components/button'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import { HoverButton } from 'components/button'
 import { Input, InputTextField } from 'components/input'
-import { Form, useFormik } from 'formik'
+import { useFormik } from 'formik'
 import { validationSchemaContact } from 'lib/validation'
 
 const ContactForm = () => {
+  const form = useRef<any>(null)
+
+  const sendEmail = () => {
+    emailjs
+      .sendForm(
+        'YOUR_SERVICE_ID',
+        'YOUR_TEMPLATE_ID',
+        form.current,
+        'YOUR_PUBLIC_KEY'
+      )
+      .then(
+        result => {
+          console.log(result.text)
+        },
+        error => {
+          console.log(error.text)
+        }
+      )
+  }
+
   const { values, handleChange, handleBlur, handleSubmit, errors, touched } =
     useFormik({
       initialValues: {
